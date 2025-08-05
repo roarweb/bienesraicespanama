7c492ed53da8b3224acf7dbb8f005d24bb97c232
Oliver Alexander <rodriguezarrocha@gmail.com>
2025-08-05 10:03:27 -0500

    art1

---
+++ propiedad_horizontal/ley284-2022.php
@@ -0,0 +1,16 @@
+$textoL2842022 = <<<EOT
+ley 284 de 14 de febrero de 2022.
+Sobre el Régimen de Propiedad Horizontal y que subroga la Ley 31 de 2010.
+
+LA ASAMBLEA NACIONAL
+DECRETA:
+
+Titulo I
+Disposiciones Generales
+
+Artículo 1. Objeto de la Ley. Se crea el Régimen de Propiedad Horizontal como un tipo especial de propiedad, con independencia funcional, en donde coexisten bienes privados con bienes comunes, con salida apropiada a la vía pública.
+
+
+EOT;
+
+<?php print($textoL2842022); ?>
